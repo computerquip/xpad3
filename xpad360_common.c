7d814194b63ab1eaf3fbf8757a939d835f088f88
Zachary Lund <admin@computerquip.com>
2014-09-06 11:48:45 -0500

    wtf git..

---
+++ xpad360_common.c
@@ -0,0 +1,98 @@
+#include <linux/usb/input.h>
+#include <linux/module.h>
+
+#include "xpad360_common.h"
+
+MODULE_AUTHOR("Zachary Lund <admin@computerquip.com>");
+MODULE_DESCRIPTION("Common Operations between Xbox 360 Devices");
+MODULE_LICENSE("GPL");
+
+struct workqueue_struct * xpad360_common_workqueue;
+
+int xpad360_setup_transfer_out(
+	struct usb_interface *usb_intf,
+	struct xpad360_transfer *transfer,
+	usb_complete_t irq)
+{
+	void * user_data = usb_get_intfdata(usb_intf);
+	struct usb_device *usb_dev = interface_to_usbdev(usb_intf);
+	struct usb_endpoint_descriptor *ep = &usb_intf->cur_altsetting->endpoint[1].desc;
+	const int pipe = usb_sndintpipe(usb_dev, ep->bEndpointAddress); 
+	int error = xpad360_alloc_transfer(usb_dev, transfer, GFP_KERNEL);
+	
+	if (error) 
+		return error;
+	
+	usb_fill_int_urb(
+		transfer->urb, usb_dev,
+		pipe, transfer->buffer, 32,
+		irq, user_data, ep->bInterval);
+	
+	return 0;
+}
+
+EXPORT_SYMBOL_GPL(xpad360_setup_transfer_out);
+
+int xpad360_setup_transfer_in(
+	struct usb_interface *usb_intf,
+	struct xpad360_transfer *transfer,
+	usb_complete_t irq)
+{
+	void * user_data = usb_get_intfdata(usb_intf);
+	struct usb_device *usb_dev = interface_to_usbdev(usb_intf);
+	struct usb_endpoint_descriptor *ep = &usb_intf->cur_altsetting->endpoint[0].desc;
+	const int pipe = usb_rcvintpipe(usb_dev, ep->bEndpointAddress); 
+	int error = xpad360_alloc_transfer(usb_dev, transfer, GFP_KERNEL);
+	
+	if (error) 
+		return error;
+	
+	usb_fill_int_urb(
+		transfer->urb, usb_dev,
+		pipe, transfer->buffer, 32,
+		irq, user_data, ep->bInterval);
+	
+	return 0;
+}
+
+EXPORT_SYMBOL_GPL(xpad360_setup_transfer_in);
+
+struct input_dev *xpad360_create_input_dev(
+	struct usb_device *usb_dev,
+	const char *name,
+	int (* open) (struct input_dev *),
+	void (* close) (struct input_dev *))
+{
+	struct input_dev *input_dev = input_allocate_device();
+	if (!input_dev) {
+		return NULL;
+	}
+	
+	usb_to_input_id(usb_dev, &input_dev->id);
+	
+	input_dev->name = name;
+	input_dev->open = open;
+	input_dev->close = close;
+	
+	return input_dev;
+}
+
+EXPORT_SYMBOL_GPL(xpad360_create_input_dev);
+
+static int __init xpad360_common_init(void)
+{
+	xpad360_common_workqueue = create_workqueue("xpad360_common");
+	
+	if (!xpad360_common_workqueue) 
+		return -ENOMEM;
+	
+	return 0;
+}
+
+static void __exit xpad360_common_exit(void)
+{
+	destroy_workqueue(xpad360_common_workqueue);
+}
+
+module_init(xpad360_common_init);
+module_exit(xpad360_common_exit);
